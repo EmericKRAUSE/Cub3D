4d45a104ee5372202872b874af79f33b9f0e32cc
Nicolas Dionis <nidionis@k1r4p5.42mulhouse.fr>
2025-03-12 11:19:32 +0100

    floodfill considers the char used for squaring the map

--- src/parsing/import_cub.c
+++ src/parsing/import_cub.c
@@ -6,7 +6,7 @@
 /*   By: nidionis <nidionis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:23:47 by ekrause           #+#    #+#             */
-/*   Updated: 2025/03/11 11:57:45 by nidionis         ###   ########.fr       */
+/*   Updated: 2025/03/12 11:15:58 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,6 +119,8 @@ int floodfill(char **map, int y, int x)
     if (is_out_of_bounds(map, y, x))
         return (FALSE);
     if (ft_strchr(BLANK_CHAR, map[y][x]))
+        return (FALSE);
+    if (map[y][x] == CHAR_BLANK_MAP)
         return (FALSE);
     if (map[y][x] == FLOODFILL_VISITED)
         return (TRUE);
